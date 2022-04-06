495cfc33038d3a557679b0ab1d2c54efe91a57cd
DONGHAEY <DONGHEAEY@gmail.com>
2022-04-06 12:19:31 +0900

    .

--- nMap/nMap.py
+++ nMap/nMap.py
@@ -3,3 +3,10 @@ import nmap
 targethost = '10.150.149.31'
 nmScan = nmap.PortScanner()
 nm = nmScan.scan(targethost, '22-80')
+
+print(nm);print()
+print(nm['nmap']);print()
+print(nm['scan']);print()
+print(nm['scan']['192.168.35.211']);print()
+print(nm['scan']['192.168.35.211']['status']);print()
+print(nm['scan']['192.168.35.211']['addresses']);print()
